0145a153e26764402947999cd06ffab28b894328
Serhey Doroshenko <prigovors@gmail.com>
2021-01-10 21:50:52 +0200

    Configure eureka discovery client

--- src/main/java/com/doroshenko/serhey/config/ConfigServerApplication.java
+++ src/main/java/com/doroshenko/serhey/config/ConfigServerApplication.java
@@ -2,8 +2,11 @@ package com.doroshenko.serhey.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@EnableConfigServer
+@SpringBootApplication(proxyBeanMethods = false)
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
